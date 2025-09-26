a201abde222da1534dd3b10067ad76e16157970f
Akash Jayaraj <akashjayarajvkp2k02@gmail.com>
2025-09-26 19:56:50 +0530

    Initial Commit

---
+++ src/test/java/com/akashjayaraj/parkingsystem/ParkingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akashjayaraj.parkingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
