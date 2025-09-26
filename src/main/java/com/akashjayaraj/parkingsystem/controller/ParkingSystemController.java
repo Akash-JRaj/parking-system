f1a2c4ec7b02caf7212df01f170ebb4ff7a340de
Akash Jayaraj <akashjayarajvkp2k02@gmail.com>
2025-09-26 21:44:36 +0530

    Added controller with endpoints, enter, exit, slots. slots - will give the available slots. Exit takes the ticket as input.

---
+++ src/main/java/com/akashjayaraj/parkingsystem/controller/ParkingSystemController.java
@@ -0,0 +1,45 @@
+package com.akashjayaraj.parkingsystem.controller;
+
+import com.akashjayaraj.parkingsystem.service.EntryGate;
+import com.akashjayaraj.parkingsystem.service.ExitGate;
+import com.akashjayaraj.parkingsystem.model.ParkingSlot;
+import com.akashjayaraj.parkingsystem.model.ParkingTicket;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ParkingSystemController {
+
+    private EntryGate entryGate;
+    private ExitGate exitGate;
+
+    public ParkingSystemController(EntryGate entryGate, ExitGate exitGate) {
+        this.entryGate = entryGate;
+        this.exitGate = exitGate;
+    }
+
+    @GetMapping("/enter")
+    public ResponseEntity<ParkingTicket> enter() {
+        ParkingTicket ticket = entryGate.getTicket();
+
+        return ResponseEntity.status(HttpStatus.OK).body(ticket);
+    }
+
+    @PostMapping("/exit")
+    public ResponseEntity<String> exit(@RequestBody ParkingTicket ticket) {
+        String response = exitGate.acceptTicket(ticket);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/slots")
+    public ResponseEntity<List<ParkingSlot>> getAvailableSlots() {
+        return ResponseEntity.status(HttpStatus.OK).body(entryGate.getAvailableSlots());
+    }
+}
